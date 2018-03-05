df422e6ea43015afd8f442783b2f5a047556831e
Bundit Charoensak <whytobe@gmail.com>
2018-03-05 10:56:48 +0700

    update build script

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,7 +13,7 @@ class App extends Component {
             <h1 className="App-title">Welcome to React</h1>
           </header>
           <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload. 3
+            To get started, edit <code>src/App.js</code> and save to reload.
           </p>
           <div>
             <Header />
